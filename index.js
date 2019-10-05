3c3274cffffc3ab97355f00173c2e3f88ba048fa
凉凉 <xyqfer@gmail.com>
2019-10-05 16:11:14 +0800

    add unitSeparator from bytes format

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const process = require('process');
 const bytes = require('bytes');
 
 module.exports = () => {
+    const bytesConfig = {
+        unitSeparator: ' ',
+    };
     const processMemoryUsage = Object.entries(process.memoryUsage()).map(([ name, value, ]) => {
         return {
-            [name]: bytes(value),
+            [name]: bytes(value, bytesConfig),
         };
     });
 
@@ -18,8 +21,8 @@ module.exports = () => {
             cpus: os.cpus(),
             LEANCLOUD_AVAILABLE_CPUS: process.env.LEANCLOUD_AVAILABLE_CPUS,
             endianness: os.endianness(),
-            totalmem: bytes(os.totalmem()),
-            freemem: bytes(os.freemem()),
+            totalmem: bytes(os.totalmem(), bytesConfig),
+            freemem: bytes(os.freemem(), bytesConfig),
             homedir: os.homedir(),
             tmpdir: os.tmpdir(),
             hostname: os.hostname(),
